5c3e521f70a26a4ec58c4b884092a88646a26514
tinito <martino.migliavacca@gmail.com>
2015-05-12 16:14:46 +0200

    fixed typo

--- core/setup.bash
+++ core/setup.bash
@@ -1,4 +1,5 @@
-export R2P_ROOT=/opt/r2p
+export R2P_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)
+#export R2P_ROOT=/opt/r2p
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$R2P_ROOT/tools/libs
 export PATH=$PATH:$R2P_ROOT/tools/gcc-arm-none-eabi-4_9-2014q4/bin:$R2P_ROOT/tools/repo
